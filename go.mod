14530e9fc77fd5c0ab4214dc3210f7b690e4ef73
olegy2010 <olegy2010@ya.ru>
2022-02-21 23:12:08 +0300

    Calculate square

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/olegy2010/golang-united-school-homework-2
+
+go 1.17
--- solution.go
+++ solution.go
@@ -1,5 +1,17 @@
 package square
 
+import (
+	"math"
+)
+
+type SidesNum int
+
+const (
+	SidesTriangle SidesNum = 3
+	SidesSquare   SidesNum = 4
+	SidesCircle   SidesNum = 0
+)
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +20,15 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+func CalcSquare(sideLen float64, sidesNum SidesNum) float64 {
+	switch sidesNum {
+	case SidesTriangle:
+		return math.Sqrt(3.0) * sideLen * sideLen / 4.0
+	case SidesSquare:
+		return sideLen * sideLen
+	case SidesCircle:
+		return math.Pi * sideLen * sideLen
+	default:
+		return 0
+	}
 }
